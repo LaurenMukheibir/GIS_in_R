19a4c4d15db5749e03663d14795de7bdf90ed9fa
Lauren Mukheibir <MKHLAU004@myuct.ac.za>
2025-03-03 10:14:34 +0200

    Data Analysis

---
+++ Scripts/data_analysis_gis.R
@@ -0,0 +1,9 @@
+#GIS in R
+#Shyshark Distribution across the Western Cape
+#Lauren Mukheibir 
+#02/03/2025
+
+#installing packages
+install.packages(tidyverse)
+install.packages(rinat)
+install.packages(sf)
